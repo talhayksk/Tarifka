d0c95b6de17f7b1726d82659849e917b158a0684
TLHYKSK46 <yuksek_talha@hotmail.com>
2021-12-13 22:02:39 +0300

    youtube izle butonu eklendi.

--- src/page/MealDetail/MealDetail.js
+++ src/page/MealDetail/MealDetail.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {View,Image,StyleSheet,Text,Dimensions,ScrollView} from  'react-native'
+import {View,Image,StyleSheet,Text,Dimensions,ScrollView,TouchableOpacity} from  'react-native'
 import Config from "react-native-config";
 import useFetch from '../../hooks/useFetch/useFetch'
 const deviceSize = Dimensions.get('window');
@@ -13,9 +13,15 @@ export default MealDetail=({route})=>{
         <ScrollView>
               <View style={styles.container}>
                 <Image style={styles.image} source={{uri:data?.meals[0]?.strMealThumb}} />
+             
                 <View style={styles.body_container}>
                 <Text style={styles.title}>{data?.meals[0]?.strMeal}</Text>
+                <TouchableOpacity style={styles.button} onPress={()=> Linking.openURL(data.meals[0].strYoutube)}>
+                    <Text style={styles.button_text} >Youtube'da İzle</Text>
+                </TouchableOpacity>
                 <Text style={styles.desc}>{data?.meals[0]?.strInstructions}</Text> 
+           
+
                 </View>
         </View>
         </ScrollView>
@@ -41,6 +47,17 @@ const styles=StyleSheet.create({
 padding:5,
 fontSize:16,
 
+    },
+    button: {
+        alignSelf:"center",
+        backgroundColor:"#d50000",
+        color:'white',
+        width:deviceSize.width/1.5,
+        height:50,
+        borderRadius:5,
+        justifyContent:"center",
+        alignItems:"center",
+        margin:5,
     },
  
 })
